daf79abafa5d4dbecc3e0c03d643a1b3b0ccdc08
Michael Kiper <mak1028@berkeley.edu>
2021-11-10 16:32:49 -0800

    route change 1

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rottenpotatoes::Application.routes.draw do
     # resources :to => redirect('/movies/search_tmdb.html.erb')
     # resources :search_tmdb
     get 'search_tmdb', to: 'movies#search_tmdb'
-    #post 'search_tmdb', to: 'movies#search_tmdb'
+    post 'search_tmdb', to: 'movies#search_tmdb'
     post "add_movie", to: 'movies#add_movie'
   end
