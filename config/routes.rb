6b00fd7acf0e64ac4a87f47ad567f45052642e4a
Michael Kiper <mak1028@berkeley.edu>
2021-11-10 22:28:50 -0800

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rottenpotatoes::Application.routes.draw do
     # map '/' to be a redirect to '/movies'
     root :to => redirect('/movies')
     # resources :to => redirect('/movies/search_tmdb.html.erb')
-    # resources :search_tmdb
-    get 'search_tmdb', to: 'movies#search_tmdb'
-    post 'search_tmdb', to: 'movies#search_tmdb'
+    get '/search_tmdb', to: 'movies#search_tmdb'
+    # post 'search_tmdb', to: 'movies#search_tmdb'
     post "add_movie", to: 'movies#add_movie'
   end
